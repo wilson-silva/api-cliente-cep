e69e60b10c15b7a66933c6fd2fb1aaabbf87add2
Wilson <wilson-gs@hotmail.com>
2022-01-25 20:28:04 -0300

    Criando repositorio

---
+++ src/main/java/one/digitalinnovation/gof/model/Endereco.java
@@ -0,0 +1,25 @@
+package one.digitalinnovation.gof.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Endereco {
+
+    @Id
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String igbe;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
